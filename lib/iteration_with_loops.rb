43ecada7065c133b23d7c4f0b0c5003727b72291
Mimi Ojserkis <mimioj@gmail.com>
2020-06-30 21:54:25 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,7 +2,6 @@ def join_nested_strings(messy_array)
   clean_array = []
   row_count = 0
   while row_count < messy_array.count do
-    # print messy_array[row_count]
     element_count = 0
   while element_count < messy_array[row_count].count do
    if messy_array[row_count][element_count].class == String
